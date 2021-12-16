47926c94d01cf8419dfcbb310b29e3210d63f9ae
Satou Fumiya <c119115@g.koeki-u.ac.jp>
2021-12-16 21:33:42 +0900

    change:大きさ変更

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-// 現在地の取得
-function GetLocation() {
+// // 現在地の取得
+// function GetLocation() {
+//     navigator.geolocation.getCurrentPosition(AddLocation);
+//     // navigator.geolocation.watchPosition(AddLocation);
+// }
+
+// function AddLocation(position) {
+//     let geo_lat = position.coords.latitude;
+//     let geo_lng = position.coords.longitude;
+//     $('a-scence').append($("<a-entity gps-entity-place=latitude:" + geo_lat + "; longitude:" + geo_lng + ";look-at=[gps-camera] scale = 1 1 1 potion=0 3 0 animation=property:rotation; dur:10000; from : 0 0 0; to : 0 360 0; loop : -1 ; easing:linear; gltf-model=#char></a-entity>"));
+//     // $('body').append($("<a-entity gps-entity-place=latitude:" + geo_lat + "; longitude:" + geo_lng + ";look-at=[gps-camera] scale = 1 1 1 potion=0 3 0 animation=property:rotation; dur:10000; from : 0 0 0; to : 0 360 0; loop : -1 ; easing:linear;gltf-model=#char></a-entity>" ));
+//     // $(entity).attr('gps-entity-place',"latitude:" + geo_lat + "; longitude:" + geo_lng +";");
+//     let entity = document.querySelector('a-entity');
+//     // console.log(entity.getAttribute('gps-entity-place'));
+//     console.log(entity);
+// }
+
+   // 現在地の取得
+   function GetLocation() {
     navigator.geolocation.getCurrentPosition(AddLocation);
     // navigator.geolocation.watchPosition(AddLocation);
 }
 function AddLocation(position) {
     // let GpsEntityPlace = entity.getAttribute('gps-entity-place');
-    let geo_lat = position.coords.latitude;
-    let geo_lng = position.coords.longitude;
+    let geo_lat = position.coords.latitude-0.00001;
+    let geo_lng = position.coords.longitude-0.00001;
     // $('a-scene').appendChild($("<a-entity gps-entity-place=latitude:" + geo_lat + "; longitude:" + geo_lng + ";look-at=[gps-camera] scale = 1 1 1 potion=0 3 0 animation=property:rotation; dur:10000; from : 0 0 0; to : 0 360 0; loop : -1 ; easing:linear;gltf-model=#char></a-entity>"));
     $('a-assets').after($("<a-entity gps-entity-place=latitude:" + geo_lat + "; longitude:" + geo_lng + ";look-at=[gps-camera] scale =1,1,1 potion=0,3,0 animation=property:rotation;dur:10000;from:0,0,0;to:0,360,0;loop:-1;easing:linear; gltf-model=#char></a-entity>" ));
     let entity = document.querySelector('a-entity');
@@ -16,6 +33,9 @@ function AddLocation(position) {
 }
 
 GetLocation();
+// $('a-scene').appendChild($("<a-entity gps-entity-place=latitude:" + geo_lat + "; longitude:" + geo_lng + ";look-at=[gps-camera] scale = 1 1 1 potion=0 3 0 animation=property:rotation; dur:10000; from : 0 0 0; to : 0 360 0; loop : -1 ; easing:linear;gltf-model=#char></a-entity>"));
+
+// GetLocation();
 
 window.addEventListener("load", () => {
     const btn = document.querySelectorAll('button');
